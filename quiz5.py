149aab63e40a47168fb119df5747d57ab1897304
s3yox <seyox055@gmail.com>
2022-09-02 00:13:50 +0300

    Create quiz5.py

---
+++ quiz5.py
@@ -0,0 +1,5 @@
+from math import sqrt
+kısak = float(input("Gısa kenarın uzunluğunu giriniz (A): "))
+uzunk = float(input("Uzun kenarın uzunluğunu giriniz (B): "))
+hipotenüs=sqrt(kısak**2 + uzunk**2)
+print(hipotenüs)
