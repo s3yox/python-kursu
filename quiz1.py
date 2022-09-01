a3d79974af13816083ff6539120a776a9043577c
s3yox <seyox055@gmail.com>
2022-09-01 21:12:20 +0300

    Create quiz1.py

---
+++ quiz1.py
@@ -0,0 +1,6 @@
+print("Uşağum ha puraya sayuları cir pakayım")
+x = int(input("1.sayıyı giriniz: "))
+y = int(input("2.sayıyı giriniz: "))
+z = int(input("3.sayıyı giriniz: "))
+carpim = x*y*z
+print("Sonuç= {}".format(carpim))
