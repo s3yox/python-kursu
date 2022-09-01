70250e0a032c51071551da00673f7da4fe27eadd
s3yox <seyox055@gmail.com>
2022-09-01 21:41:10 +0300

    Create quiz2.py

---
+++ quiz2.py
@@ -0,0 +1,6 @@
+print("Usta şimdi senin bmı hesabını yapıcaz.")
+#LEYLİM LEYY
+boy = float(input("Boyunuzu girer misiniz zahmet olmazsa (metre cinsinden :) : "))
+kilo = float(input("Kaç kilosunuz acaba: "))
+indeks = kilo/boy**2
+print("Usta senin bmı: {}".format(indeks))
