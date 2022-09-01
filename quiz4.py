30dd87e7d42d82b8a10253c7e9a30cf6e92eff38
s3yox <seyox055@gmail.com>
2022-09-02 00:07:01 +0300

    Create quiz4.py

---
+++ quiz4.py
@@ -0,0 +1,8 @@
+a = int(input("Aklından ondalık olmayan bir sayı tut: "))
+b = int(input("Aklından ondalık olmayan bir sayı daha tut: "))
+c=b
+b=a
+a=c
+
+
+print("ilk bu {} sonra  {} aklından geçen sayıalrdı değil mi? \n Bilemedim mi :(".format(a, b))
