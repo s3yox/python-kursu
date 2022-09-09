c124ec353eba80fd9602fbc34160a5c508586348
s3yox <101594645+s3yox@users.noreply.github.com>
2022-09-09 21:29:45 +0300

    Update quiz5.py

--- quiz5.py
+++ quiz5.py
@@ -2,4 +2,4 @@ from math import sqrt
 kısak = float(input("Gısa kenarın uzunluğunu giriniz (A): "))
 uzunk = float(input("Uzun kenarın uzunluğunu giriniz (B): "))
 hipotenus=sqrt(kısak**2 + uzunk**2)
-print(hipotenüs)
+print(hipotenus)
