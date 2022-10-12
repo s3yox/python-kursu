02c78b43a80ba5c7bb196cb738248635ee9c707f
s3yox <seyox055@gmail.com>
2022-10-12 22:34:13 +0300

    Update ödev2.py

--- conditionals/ödev2.py
+++ conditionals/ödev2.py
@@ -2,20 +2,20 @@ geosekil=(input("Hangi geometrik şekilden bahsediyorsunuz:"))
 dortgen=("Dörtgen")
 ucgen=("Üçgen")
 if geosekil == dortgen :
-    kenar1=int(input("1.kenarı giriniz:"))
-    kenar2=int(input("2.kenarı giriniz:"))
-    kenar3=int(input("3.kenarı giriniz:"))
-    kenar4=int(input("4.kenarı giriniz:"))
+    a=int(input("1.kenarı giriniz:"))
+    b=int(input("2.kenarı giriniz:"))
+    c=int(input("3.kenarı giriniz:"))
+    d=int(input("4.kenarı giriniz:"))
 
-    if (kenar1==kenar2) and (kenar2==kenar3) and (kenar3==kenar4) and (kenar4==kenar1):
+    if (a==b) and (b==c) and (c==d) and (d==a):
         print("Bu bir kare")
     else: 
         print("Bu bir dörtgen")
 elif geosekil == ucgen:
- kenar1=int(input("kenar1 giriniz:"))
- kenar2=int(input("Kenar2 giriniz:"))
- kenar3=int(input("kenar3 giriniz:"))
-if (kenar1==kenar2) and (kenar2==kenar3):
-  print("Bu bir eşkenar")
-
-
+ a=int(input("a giriniz:"))
+ b=int(input("b giriniz:"))
+ c=int(input("c giriniz:"))
+ if (a==b) and (b==c):
+   print("Bu bir eşkenar")
+ elif (a==b) and (a!=c):
+     print("Bu ikizkenar")
