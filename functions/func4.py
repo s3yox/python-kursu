98902b62f1355fb35e8efd3e35a4d0f7554779d9
s3yox <seyox055@gmail.com>
2022-10-27 00:52:26 +0300

    Create func4.py

---
+++ functions/func4.py
@@ -0,0 +1,14 @@
+def ebob_bul(sayi1,sayi2):
+    if(sayi1>sayi2):
+        i=sayi2
+    else:
+        i=sayi1
+
+    while(i>0):
+        if(sayi1%i == 0 and sayi2%i==0):
+            return i
+        i-=1
+    return 1
+sayi1=int(input("ilk sayıyı girin: "))
+sayi2=int(input("ikinci sayıyı girin: "))
+print("ebob =",ebob_bul(sayi1,sayi2))             
